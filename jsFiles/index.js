8d36e2cbe2487f8ee5efc09c69a7a87852276881
valitovgaziz <valitovgaziz@gmail.com>
2023-01-22 10:54:59 +0500

    Сделал все и решил сделать хотя бы раз коммит)))

---
+++ jsFiles/index.js
@@ -0,0 +1,7 @@
+let search_request = function() {
+    let search_text = document.getElementById("search_input")
+    alert("Сервис находиться в стадии сбора информации.");
+};
+
+
+document.getElementById("search_form").addEventListener("submit", search_request);
