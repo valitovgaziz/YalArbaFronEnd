79c7ce31c9b7c0d05a677f62a6afa50b9c5dc9e3
valitovgaziz <valitovgaziz@gmail.com>
2023-01-28 07:58:23 +0500

    Добавлена страница результатов поискового запроса.

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -1,7 +1,5 @@
 let search_request = function() {
-    let search_text = document.getElementById("search_input")
-    alert("Сервис находиться в стадии сбора информации.");
+    window.location.replace("pages/searchResult.html");
 };
-
 
 document.getElementById("search_form").addEventListener("submit", search_request);
