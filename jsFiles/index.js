86c8ca8c9eefb294d21a83af7ef4083f4ccdc41f
valitovgaziz <valitovgaziz@gmail.com>
2023-01-28 17:00:56 +0500

    Почти чделал выпадающее меню.

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -1,5 +1,17 @@
-let search_request = function() {
-    window.location.replace("pages/searchResult.html");
-};
+window.onload = function () {
+    let search_request = function () {
+        window.location.replace("pages/searchResult.html");
+    };
 
-document.getElementById("search_form").addEventListener("submit", search_request);
+    let searchForm = document.querySelector('#search_form');
+    searchForm.addEventListener(
+        "submit",
+        search_request
+    );
+
+    let menuToggle = document.querySelector('.menuToggle');
+    let navigation = document.querySelector('.navigation');
+    menuToggle.onclick = function () {
+        navigation.classList.toggle('active');
+    }
+}
