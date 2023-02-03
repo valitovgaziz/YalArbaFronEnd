30bbb6aeb01d9ddfe07a683404464c66d57a4686
valitovgaziz <valitovgaziz@gmail.com>
2023-02-03 11:07:10 +0500

    Еще более стилизованная страница.

--- jsFiles/addObject.js
+++ jsFiles/addObject.js
@@ -1,11 +1,11 @@
 window.onload = function () {
 
+    let radioButton = document.querySelector('label[type=radio]');
+
     let menuToggle = document.querySelector('.menuToggle');
     let navigation = document.querySelector('.navigation');
     menuToggle.onclick = function () {
         navigation.classList.toggle('active');
     }
-
-
 
 }
