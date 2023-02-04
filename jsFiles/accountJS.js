1760898fb45bb563342fff7f8ff50b349f1789e7
valitovgaziz <valitovgaziz@gmail.com>
2023-02-04 14:05:17 +0500

    Save

--- jsFiles/accountJS.js
+++ jsFiles/accountJS.js
@@ -5,4 +5,21 @@ window.onload = function () {
     menuToggle.onclick = function () {
         navigation.classList.toggle('active');
     }
+
+    document.querySelector('.editButton')
+        .addEventListener('click', function() {
+            window.location.href = 'addObjectPage.html';
+        });
+
+    document.querySelector('.deleteButton')
+        .addEventListener('click', function () {
+            let deleteButton = document.querySelector('.deleteSubForm');
+
+            alert(deleteButton.getAttribute('visibility'));
+
+            deleteButton.setAttribute('visibility', 'visible');
+
+            deleteButton.visibility = 'visible';
+        });
+
 }
