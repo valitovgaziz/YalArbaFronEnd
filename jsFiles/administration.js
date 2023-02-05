774f9bd18f78d34303b59f8182c65ab7a98e7f27
valitovgaziz <valitovgaziz@gmail.com>
2023-02-05 20:58:42 +0500

    Стилизовал кнопки в модальном окне страница администрирования аккаунтов.

--- jsFiles/administration.js
+++ jsFiles/administration.js
@@ -1,21 +1,43 @@
 window.onload = function () {
 
-    document.querySelector('.editButton')
-        .addEventListener('click', function () {
-            document.querySelector('.dialog')
-                .showModal();
+    let editButtons = document.querySelectorAll('.editButton');
+
+
+    editButtons.forEach(function (nextVal, index, nodes) {
+        nextVal.addEventListener('click', function () {
+            let modalDialog = document.querySelector('.dialog');
+            modalDialog.showModal();
         });
+    });
 
     document.querySelector('.deleteButton')
         .addEventListener('click', function () {
             document.querySelector('.dialog')
                 .close();
-        })
+        });
 
     document.querySelector('.verifyButton')
         .addEventListener('click', function () {
             document.querySelector('.dialog')
                 .close();
-        })
+        });
+
+    document.querySelector('.banButton')
+        .addEventListener('click', function () {
+            document.querySelector('.dialog')
+                .close();
+        });
+
+    document.querySelector('.unBanButton')
+        .addEventListener('click', function () {
+            document.querySelector('.dialog')
+                .close();
+        });
+
+    document.querySelector('.cancel')
+        .addEventListener('click', function () {
+            document.querySelector('.dialog')
+                .close();
+        });
 
 }
